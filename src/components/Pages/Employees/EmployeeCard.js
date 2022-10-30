e9ba991c876e0301573130766b03db588688cc95
Yinon Bar <yinonbar1988@gmail.com>
2022-10-30 22:17:07 +0200

    fully working app with routing

---
+++ src/components/Pages/Employees/EmployeeCard.js
@@ -0,0 +1,12 @@
+import "./EmployeeCard.css";
+
+function EmployeeCard() {
+  return (
+    <div className="EmploteeCard">
+      <h1>Employee</h1>
+      <h1>Employee</h1>
+    </div>
+  );
+}
+
+export default EmployeeCard;
